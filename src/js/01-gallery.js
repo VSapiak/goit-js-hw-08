34e5f36884722d0000ea3f66730c22c38c7a9807
VSapiak <vsapiak5@gmail.com>
2023-04-07 10:50:45 +0100

    new

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,48 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+const galleryItem = document.querySelector('.gallery');
+
+const imageItem = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+	 class="gallery__image"
+	 src="${preview}"
+	 data-source="${original}"
+	 alt="${description}"
+  />
+</a>
+</li>`
+  )
+  .join('');
+
+galleryItem.insertAdjacentHTML('beforeend', imageItem);
+galleryItem.addEventListener('click', e => {
+  e.preventDefault();
+  const target = e.target;
+  if (target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const galleryImageSrc = e.target.dataset.source;
+  const galleryAltText = e.target.alt;
+
+  const instance = basicLightbox.create(`
+    <img src="${galleryImageSrc}" alt="${galleryAltText}" width="800" height="600">,
+`);
+
+  instance.show();
+
+  const escapePress = e => {
+    if (e.key === 'Escape') {
+      instance.close();
+      document.removeEventListener('keydown', escapePress);
+    }
+  };
+  document.addEventListener('keydown', escapePress);
+});
+
 console.log(galleryItems);
