d03c48291e87a6ac51a635481ce1b5403a12e286
VSapiak <vsapiak5@gmail.com>
2023-04-18 13:41:02 +0100

    2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,35 +1,41 @@
 var throttle = require('lodash.throttle');
 
 const emailForm = document.querySelector('.feedback-form');
-const emailInput = emailForm.querySelector('input[name="email"]');
-const messageInput = emailForm.querySelector('textarea[name="message"]');
+const emailFormInput = emailForm.querySelector(`input[name="email"]`);
+const messageFormInput = emailForm.querySelector(`textarea[name="message"]`);
 const FORM_KEY = "feedback-form-state";
-const formObject = {};
 
 emailForm.addEventListener('submit', clearFormSubmit);
 emailForm.addEventListener('input', throttle(addTextInput, 500));
 saveSubmitTextrea();
 
-
 // // очищаємо форму, видаляємо з application
 function clearFormSubmit(e) {
 	e.preventDefault();
-	e.target.reset()
+	console.log({
+		email: emailFormInput.value,
+		message: messageFormInput.value
+	});
+	e.target.reset();
 	localStorage.removeItem(FORM_KEY);
 };
 
 // функція в яку получаємо значення, за збеерігаємо до application
- function addTextInput(e){
-	formObject[e.target.name] = e.target.value;
+ function addTextInput() {
+	const formObject = {
+		email: emailFormInput.value,
+		message: messageFormInput.value
+	};
 	localStorage.setItem(FORM_KEY, JSON.stringify(formObject));
+	console.log(formObject);
 };
 
 // function save elements and remove emelents of application
 function saveSubmitTextrea() {
-	 const saveElement = localStorage.getItem(FORM_KEY);
-	 if(saveElement) {
-		const dateEmelent = JSON.parse(saveElement);
-		emailInput.value = dateEmelent.email;
-		messageInput.value = dateEmelent.message;
-	 }
+	const savesElemsntToApplication = localStorage.getItem(FORM_KEY);
+	if (savesElemsntToApplication) {
+		const save = JSON.parse(savesElemsntToApplication);
+		emailFormInput.value = save.email;
+		messageFormInput.value = save.message;
+	}
 };
